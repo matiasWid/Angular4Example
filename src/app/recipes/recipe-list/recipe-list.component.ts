96d320f9e61db68f366958821e9d3952e9cf8955
Matias Widmaier <matiaswidmaier92@gmail.com>
2017-04-07 12:53:21 -0300

    adding routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -9,10 +10,11 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute) {
 
   }
 
@@ -20,8 +22,8 @@ export class RecipesListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  onNewRecipe(){
+    this.router.navigate(['new'], { relativeTo: this.route});
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,44 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService{
-    private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This a simple test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
-        new Recipe('A another recipe', 'This a simple test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
-  ];  
+	recipeSelected = new EventEmitter<Recipe>();
 
-  getRecipes() {
-      return this.recipes.slice();
-  }
+	private recipes: Recipe[] = [
+		new Recipe(
+			'Tasty Schnitzel', 
+			'A super-tasty Schnitzel - just awesome!', 
+			'http://givememora.com/wp-content/uploads/2014/03/shnitzel.jpg', 
+			[
+				new Ingredient('Meat',1),
+				new Ingredient('French Fires', 20),
+			]),
+		new Recipe(
+			'Big Fat Burger', 
+			'What else you need say?', 
+			'http://www.fatburgercanada.com/wp-content/uploads/2015/07/fatburger_0001_DoubleFatCheese-541x633.png',
+			[
+				new Ingredient('Buns', 2),
+				new Ingredient('Meat', 1)
+			])
+	];  
+
+	constructor(private slService: ShoppingListService) {}
+
+	getRecipes() {
+		return this.recipes.slice();
+	}
+
+	getRecipe(index: number) {
+		return this.recipes[index];
+	}
+
+	addIngredientsToShoppingList(ingredients: Ingredient[]) {
+		this.slService.addIngredients(ingredients);
+	}
+
 }
